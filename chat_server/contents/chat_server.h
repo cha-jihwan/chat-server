1738c10728844abad8b41501c37c8a844a9c47e2
cha-jihwan <79242140+cha-jihwan@users.noreply.github.com>
2021-02-25 12:29:56 +0900

    update commnets
    주석 기능

--- chat_server/contents/chat_server.h
+++ chat_server/contents/chat_server.h
@@ -2,20 +2,30 @@
 
 namespace c2 { namespace server { namespace contents
 {
-	class chat_server : public c2::server::core::select_server 
+	using namespace c2::server::core;
+
+	// 채팅 서버.
+	class chat_server : public select_server 
 	{
 	public:
+		// 
 		chat_server();
 		virtual ~chat_server();
 
+		// 채팅 서버에 필요한 초기화를 담당하는 함수.
 		virtual bool initialize_contents();
+		
+		// user와 sesion에 대한 할당 함수.
 		virtual session* allocate_session() override final;
 		virtual i_user* allocate_user(const string& user_name) override final;
 
+		// user와 session에 대한 할당 해제 함수.
 		virtual void free_session(session* sess) override final; 
 		virtual void free_user(i_user* user)     override final;
 
+		// session이 accept()된 이후의 콘텐츠단에서 필요한 처리
 		virtual void on_accept(session* sess)	override final;
+		// session이 연결이 끊긴 이후의 콘텐츠단에서 필요한 처리
 		virtual void on_disconnect(session* sess)	override final;
 
 		// user 를 active_table에 추가.
@@ -26,12 +36,18 @@ namespace c2 { namespace server { namespace contents
 		// 삽입이 되었다면 true를 리턴.
 		void unregister_user(chat_user* user);
 
+		// 
 		chat_user*	get_user(const string& name);
+
+		// 현재 활성화된 유저들의 목록을 
+		// "user_name\r\n" 포맷의 string객체를 통해 반환
 		string		get_active_user_to_string();
 
 	private:
+		// 현재 할당된 user의 name을 키로 하는 user객체 관리 테이블.
 		unordered_map<string, chat_user*>		active_user_table;
 
+		// session과 user의 오브젝트 풀.
 		bounded_object_pool<chat_session, constant::c_maximum_ccu>*	session_pool;
 		bounded_object_pool<chat_user, constant::c_maximum_ccu> *	user_pool;
 	};
--- chat_server/core/object_pool.hpp
+++ chat_server/core/object_pool.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
 // 객체 크기가 제한된 오브젝트 풀.
-
+// 싱글 스레드에서 재할당이 없다는 
 // 주의 사항
 // 만일 스택 할당시 stack 사이즈가 1mb가 넘는 상황이 예상시. 동적 할당이 권장된다. 
-// vector를 stack 처럼 사용한다. 객체간 붙여서 캐시 지역성을 고려.
+// vector를 stack 처럼 사용한다.	// 최근 할당 해제된 객체 재사용 -> 캐시 시간 지역성 고려
+// 객체간 붙인다.													-> 캐시 지역성을 고려.
 
 using namespace std;
 template <typename T, size_t N>
 class bounded_object_pool
 {
 public:
+	// 생성자.
 	bounded_object_pool();
+
+	// 객체를 할당후 생성자를 통해 초기화하고 반환한다.
 	T* allocate();
+	// 객체를 풀에 반환. 
 	void free(T* obj);
 	const size_t capacity();
 	const size_t size() const;
@@ -29,7 +34,6 @@ template <typename T, size_t N>
 bounded_object_pool<T, N>::bounded_object_pool()
 {
 	//static_assert( (sizeof(T) * N) >= (1024 * 1024), "" );
-
 	pool_container.reserve(N);
 
 	for (T& obj : objects)
@@ -46,8 +50,11 @@ T* bounded_object_pool<T, N>::allocate()
 		return nullptr;
 	}
 
+	// 제일 최근에 추가된 객체를 가져온다. 
+	// 캐시의 시간 지역성 고려.
 	T* element = pool_container.back();
 
+	// 벡터 뒤의 원소를 제거.
 	pool_container.pop_back();
 
 	// 생성자 호출.
@@ -57,11 +64,13 @@ T* bounded_object_pool<T, N>::allocate()
 	return element;
 }
 
+// 객체를 반환하고 소멸자를 호출한다.
 template <typename T, size_t N>
 void bounded_object_pool<T, N>::free(T* obj)
 {
 	crash_if_false(nullptr != obj);
 
+	// 소멸자 호출.
 	obj->~T();
 
 	return pool_container.push_back(obj);
